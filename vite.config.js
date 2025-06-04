3621838227b625510bd0f906da3122c02a145bf2
yongkang he <42984858-yongkanghe@users.noreply.replit.com>
2025-06-04 08:42:06 +0000

    Restored to '7d3e6fbc2a1349d2ab4fbdacfba5bfb8c8043b42'
    Replit-Restored-To: 7d3e6fbc2a1349d2ab4fbdacfba5bfb8c8043b42

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: 'all',
-    hmr: {
-      clientPort: 443
-    }
-  }
 })
